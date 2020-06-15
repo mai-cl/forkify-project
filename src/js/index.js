60cff09c57cf4bb4318f6eac386e2c2ed8f62a28
Rodrigo Canaviri <rodrigomt.cl@gmail.com>
2020-06-15 16:21:52 -0300

    add loading spinner

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView'
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
  * - Search object
@@ -22,12 +22,14 @@ const controlSearch = async () => {
         // 3- Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // 4- Search for recipes
         await state.search.getResults();
 
         // 5- Render results on UI
         //console.log(state.search.result);
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 }
@@ -35,6 +37,17 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    //console.log(btn);
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+        //console.log(goToPage);
+    }
 })
 
 /*
