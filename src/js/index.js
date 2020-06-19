8c8b075de1f9e720f16e40888fe204f43247b757
Rodrigo Canaviri <rodrigomt.cl@gmail.com>
2020-06-19 01:24:26 -0300

    add likes model and controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView'
 import * as recipeView from './views/recipeView'
 import * as listView from './views/listView'
@@ -53,8 +54,6 @@ elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
-
-
 
 elements.searchResPages.addEventListener('click', e => {
     const btn = e.target.closest('.btn-inline');
@@ -163,6 +162,41 @@ elements.shopping.addEventListener('click', e => {
 
 })
 
+
+
+/**
+ * LIKE CONTROLLER
+ */
+const controlLike = () => {
+    if (!state.likes) state.likes = new Likes();
+    const currentID = state.recipe.id;
+
+    //user has not yet liked current recipe
+    if (!state.likes.isLiked(currentID)) {
+        //add like to the state
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+        )
+        //toggle the like button
+
+        //add like to UI list
+        console.log(state.likes);
+    //user has liked current recipe
+    } else {
+        //remove like to the state
+        state.likes.deleteLike(currentID);
+
+        //toggle the like button
+
+        //remove like to UI list
+        console.log(state.likes);
+    }
+}
+
+
 //handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
     if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -174,7 +208,11 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
     } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        //add ingredients to shopping list
         controlList();
+    } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+        //like controlller
+        controlLike();
     }
 
 });
